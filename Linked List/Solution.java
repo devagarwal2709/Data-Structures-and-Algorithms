5739d90622ebc99449d431cbebb1c920e056ee61
Dev Agarwal <devagarwal2709@gmail.com>
2026-07-10 01:16:01 +0530

    Added Linked List Cycle Soltuion

---
+++ Linked List/Solution.java
@@ -0,0 +1,37 @@
+/*
+Pattern: Slow and Fast Pointer (Floyd's Cycle Detection)
+
+Time: O(n)
+Space: O(1)
+
+Idea:
+Move slow by one step and fast by two steps.
+If they ever meet, a cycle exists.
+If fast reaches null, there is no cycle.
+*/
+
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+            if(slow==fast){
+                return true;
+            }
+        }
+        return false;
+    }
+}
