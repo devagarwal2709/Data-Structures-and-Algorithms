e9af7ef3470f43df270997fec6a45bb5f26d6432
Dev Agarwal <devagarwal2709@gmail.com>
2026-07-10 21:19:25 +0530

    Added Linked List Cycle II Soltuion

---
+++ Linked List/LinkedListCycleII.java
@@ -0,0 +1,43 @@
+/*
+Pattern: Floyd's Cycle Detection (Tortoise & Hare)
+
+Time: O(N)
+Space: O(1)
+
+Idea:
+First detect whether a cycle exists using slow and fast pointers.
+If they meet, reset one pointer to head.
+Move both pointers one step at a time.
+The node where they meet again is the starting node of the cycle.
+*/
+
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        ListNode slow=head;
+        ListNode fast=head;
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+            if(slow==fast){
+                slow=head;
+                while(slow!=fast){
+                    slow=slow.next;
+                    fast=fast.next;
+                }
+                return slow;
+            }
+        }
+        return null;
+    }
+}
